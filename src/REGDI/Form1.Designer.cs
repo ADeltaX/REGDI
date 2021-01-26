6a486bc7010920c2def8e4e411b4d60e2f2ed801
ADeltaX <info@adeltax.com>
2021-01-26 20:45:21 +0100

    src

---
+++ src/REGDI/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿
+namespace REGDI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(243, 249);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Wait.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 600);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "REGDI++;";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ src/REGDI/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace REGDI
+{
+    public partial class Form1 : Form
+    {
+        Timer t;
+
+        static int f = 0;
+        static Random rnd = new Random();
+        static int size = 6;
+        static int columnGrid = 99;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void T_Tick(object sender, EventArgs e)
+        {
+            this.Invoke(new Action(() =>
+            {
+                this.Text = "Creating controls....";
+                Stopwatch sp = Stopwatch.StartNew();
+                Controls.Clear();
+                for (int i = 0; i < columnGrid; i++)
+                {
+                    for (int j = 0; j < columnGrid + 1; j++) // 100 controls instead of 99
+                    {
+                        Control b = new Control();
+                        b.Height = size;
+                        b.Width = size;
+                        b.Padding = Padding.Empty;
+                        b.Margin = Padding.Empty;
+                        //Random may slow down a bit, but it's for visual
+                        //To test controls generation speed properly, please set a fixed color.
+                        b.BackColor = Color.FromArgb(rnd.Next(0, 256), rnd.Next(0, 256), rnd.Next(0, 256));
+                        b.Location = new Point(i * size, j * size);
+                        Controls.Add(b);
+                    }
+                }
+
+                label1.Visible = false;
+                sp.Stop();
+                this.Text = "Controls created in: " + sp.Elapsed.ToString("mm\\:ss\\.fff");
+            }));
+            t.Stop();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            t = new Timer();
+            t.Interval = 2000;
+            t.Tick += T_Tick;
+            t.Start();
+        }
+    }
+}
